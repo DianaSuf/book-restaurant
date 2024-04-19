3d55aae6f1fbd5e2fa978fa86137564f09dbedff
DianaSuf <129599628+DianaSuf@users.noreply.github.com>
2024-04-20 01:55:52 +0500

    оформила модалку

---
+++ src/components/modal/modal.jsx
@@ -0,0 +1,96 @@
+import './modal.css';
+import PropTypes from 'prop-types';
+
+ModalRegister.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+};
+
+export default function ModalRegister ({ isOpen, onClose }) {
+    const onWrapperClick = (event) => {
+        if (event.target.classList.contains("modal-wrapper")) onClose();
+    };
+
+    return (
+        <>
+            {isOpen && (
+                <div className="modal">
+                    <div className="modal-wrapper" onClick={onWrapperClick}>
+                        <div className="modal-content">
+                            <form action="#" className="register__form">
+                                <div className="register__fields">
+                                    <div className="register__field">
+                                        <label
+                                            className="register__label visually-hidden"
+                                            htmlFor="user-name"
+                                        >
+                                                    Имя пользователя
+                                        </label>
+                                    </div>
+                                    <input
+                                        className="register__input"
+                                        type="text"
+                                        name="user-name"
+                                        id="user-name"
+                                        required
+                                    />
+                                    <div className="register__field">
+                                        <label
+                                            className="register__label visually-hidden"
+                                            htmlFor="user-email"
+                                        >
+                                                    Почта
+                                        </label>
+                                    </div>
+                                    <input
+                                        className="register__input"
+                                        type="email"
+                                        name="user-email"
+                                        id="user-email"
+                                        required
+                                    />
+                                    <div className="register__field">
+                                        <label
+                                            className="register__label visually-hidden"
+                                            htmlFor="user-phone"
+                                        >
+                                                    Номер телефона
+                                        </label>
+                                    </div>
+                                    <input
+                                        className="register__input"
+                                        type="tel"
+                                        name="user-phone"
+                                        id="user-phone"
+                                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                        required
+                                    />
+                                    <div className="register__field">
+                                        <label
+                                            className="register__label visually-hidden"
+                                            htmlFor="user-password"
+                                        >
+                                                    Пароль
+                                        </label>
+                                    </div>
+                                    <input
+                                        className="register__input"
+                                        type="password"
+                                        // placeholder="Password"
+                                        name="user-password"
+                                        id="user-password"
+                                        required
+                                    />
+                                </div>
+                                <div className="sign-in__submit">
+                                    <button className="register__btn" type="submit"></button>
+                                    <button className="sign-in__btn" type="submit"></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    )
+}
--- src/pages/main-screen/main-screen.jsx
+++ src/pages/main-screen/main-screen.jsx
@@ -1,13 +1,20 @@
 import logo from '../../../public/logo.svg'
-import profile from '../../../public/profile.svg'
+// import profile from '../../../public/profile.svg'
+import ModalRegister from '../../components/modal/modal';
+import { useState } from 'react'
 import './main-screen.css'
 
 export default function MainScreen() {
+  const [modalRegIsOpen, setModalRegIsOpen] = useState(false);
+
   return (
     <>
       <div className="header">
           <a className="logo"><img className="logo_link" alt="logo" src={logo}/></a>
-          <a className="profile"><img className="profile_link" alt="profile" src={profile}/></a>
+          <button className="image-button" onClick={() => setModalRegIsOpen(true)}></button>
+          <ModalRegister
+          isOpen={modalRegIsOpen}
+          onClose={() => setModalRegIsOpen(false)}/>
       </div>
     </>
   )
