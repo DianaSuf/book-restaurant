7df4a8ceda6a2759c39ed1c2bc48467eb2a18a6c
DianaSuf <129599628+DianaSuf@users.noreply.github.com>
2024-05-31 17:17:56 +0500

    бек 3.0

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { checkAuthAction } from '../store/api-actions';
 import { useAppDispatch } from '../hooks/hook';
 // import { APIRoute } from '../const';
 
-const BACKEND_URL = 'http://generous-rejoicing-production.up.railway.app/';
+const BACKEND_URL = 'https://generous-rejoicing-production.up.railway.app/';
 // const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = () => {
